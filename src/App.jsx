4e8be68d69710296949fff43829058e31ae88099
Pallavi Tiwari <pallavi6594@gmail.com>
2023-12-26 03:13:31 -0600

    created pages and route

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,22 @@
-import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import React from 'react';
+import Home from './pages/Home'
+import About from './pages/About';
+import Profile from './pages/Profile';
+import SignIn from './pages/SignIn';
+import LogOut from './pages/LogOut'
+
 
 export default function App() {
-  return (
-    <h1 className='text-red-500'>App</h1>
+  return (<BrowserRouter>
+    <Routes>
+      <Route path='/' element={<Home />}/>
+      <Route path='/home' element={<Home />}/>
+      <Route path='/about' element={<About />}/>
+      <Route path='/profile' element={<Profile />}/>
+      <Route path='/sign-in' element={<SignIn />}/>
+      <Route path='/log-out' element={<LogOut />}/>
+    </Routes>
+  </BrowserRouter>
   )
 }
