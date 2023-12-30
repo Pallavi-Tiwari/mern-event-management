bebcc0bf69ad939bf47f1c5ccc3d310552e1ae33
Pallavi Tiwari <pallavi6594@gmail.com>
2023-12-30 01:03:26 -0600

    test api created and working

--- api/index.js
+++ api/index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
+import userRouter from './routes/user.route.js'
 dotenv.config();
 
 mongoose.connect(process.env.MONGO).then(() => {
@@ -15,3 +16,5 @@ app.listen(5000, () => {
     console.log('Server is running on port 5000');
     }
 );
+
+app.use('/api/user', userRouter);
