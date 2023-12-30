b3774f809c9de9aecf4a7c8f7414f95503e26339
Pallavi Tiwari <pallavi6594@gmail.com>
2023-12-30 02:52:03 -0600

    created middleware and errorHandler for possible errors

--- api/index.js
+++ api/index.js
@@ -24,3 +24,13 @@ app.listen(5000, () => {
 
 app.use('/api/user', userRouter);
 app.use('/api/auth', authRouter);
+
+app.use((err, req, res, next) => {
+    const statusCode = err.statusCode || 500;
+    const message = err.message || 'Internal Server Error';
+    return res.status(statusCode).json({
+        success: false,
+        statusCode,
+        message,
+    });
+});
