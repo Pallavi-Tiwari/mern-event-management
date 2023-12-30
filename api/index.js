5d9d1783bf252668221ad9ac5ff719468a5000f6
Pallavi Tiwari <pallavi6594@gmail.com>
2023-12-29 19:51:39 -0600

    Connected to Mongodb

--- api/index.js
+++ api/index.js
@@ -1,8 +1,17 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+dotenv.config();
+
+mongoose.connect(process.env.MONGO).then(() => {
+    console.log('Connected to MongoDB database');
+    }).catch((err) => {
+        console.log(err)
+    })
 
 const app = express();
 
 app.listen(5000, () => {
     console.log('Server is running on port 5000');
-}
+    }
 );
