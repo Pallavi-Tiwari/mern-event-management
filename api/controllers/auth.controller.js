72d256c079e95a5a3bf2a1556b9e02500ce8eb28
Pallavi Tiwari <pallavi6594@gmail.com>
2023-12-31 21:07:10 -0600

    Complete signup page with basic functionalities and check

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -3,13 +3,22 @@ import bcryptjs from 'bcryptjs';
 import { errorHandler } from '../utils/error.js';
 
 export const signup = async (req, res, next) => {
-    const { username, email, password} = req.body;
-    const hashedPassword = bcryptjs.hashSync(password,10);
-    const newUser = new User({username, email, password: hashedPassword});
+    const { username, email, password } = req.body;
+    if (!username) {
+        return res.status(400).json({ success: false, message: 'Username empty!' });
+    }
+    if (!email) {
+        return res.status(400).json({ success: false, message: 'Email empty!' });
+    }
+    if (!password) {
+        return res.status(400).json({ success: false, message: 'Password empty!' });
+    }
+    const hashedPassword = bcryptjs.hashSync(password, 10);
+    const newUser = new User({ username, email, password: hashedPassword });
     try {
         await newUser.save();
-        res.status(201).json('User created successfully!');
-    } catch(error) {
+        res.status(201).json({ success: true, message: 'User created successfully!' });
+    } catch (error) {
         next(error);
     }
 
