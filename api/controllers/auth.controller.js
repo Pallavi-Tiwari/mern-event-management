c0ab5e8dc677a95cdae5b3388ca50681fc28c199
Pallavi Tiwari <pallavi6594@gmail.com>
2023-12-31 23:38:58 -0600

    Sign In page functionality completed with input validation

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -30,7 +30,7 @@ export const signin = async (req, res, next)=> {
         const validUser = await User.findOne({email});
         if (!validUser) return next(errorHandler(404, 'User not found!'));
         const validPassword = bcryptjs.compareSync(password, validUser.password);
-        if (!validPassword) return next(errorHandler(401, 'Wrong credentials!'));
+        if (!validPassword) return next(errorHandler(401, 'Credentials provided are incorrect!'));
         const token = jwt.sign({id: validUser._id}, process.env.JWT_SECRET);
         const { password: passW, ...rest } = validUser._doc;
         res.cookie('access_token', token, { httpOnly: true, expires: new Date(Date.now() + 24*60*60*1000) })
