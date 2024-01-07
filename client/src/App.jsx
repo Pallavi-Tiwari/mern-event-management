993ebcd06796787236138455f68e13206788c29f
Pallavi Tiwari <pallavi6594@gmail.com>
2024-01-06 19:40:37 -0600

    Making profile page private

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,6 +6,7 @@ import Profile from './pages/Profile';
 import SignIn from './pages/SignIn';
 import SignUp from './pages/SignUp'
 import Header from './components/Header';
+import PrivateRoute from './components/PrivateRoute';
 
 
 export default function App() {
@@ -15,7 +16,9 @@ export default function App() {
         <Route path='/' element={<Home />}/>
         <Route path='/home' element={<Home />}/>
         <Route path='/about' element={<About />}/>
+        <Route element={<PrivateRoute />}>
         <Route path='/profile' element={<Profile />}/>
+        </Route>
         <Route path='/sign-in' element={<SignIn />}/>
         <Route path='/sign-up' element={<SignUp />}/>
       </Routes>
--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,7 +1,9 @@
 import {FaSearch} from 'react-icons/fa';
 import {Link} from 'react-router-dom';
+import { useSelector } from 'react-redux';
 
 export default function Header() {
+    const { currentUser } = useSelector(state => state.user);
   return (
     <header className='bg-slate-200 shadow-md'>
         <div className='flex justify-between items-center max-w-6xl mx-auto p-3'>
@@ -20,7 +22,11 @@ export default function Header() {
             <ul className='flex gap-4'>
                 <Link to='/home'><li className='hidden sm:inline text-slate-700 hover:underline cursor-pointer'>Home</li></Link>
                 <Link to='/about'><li className='hidden sm:inline text-slate-700 hover:underline cursor-pointer'>About</li></Link>
-                <Link to='/sign-in'><li className='text-slate-700 hover:underline cursor-pointer'>Sign in</li></Link>
+                <Link to='/profile'>
+                    {currentUser ? (
+                        <img className='text-slate-700 h-7 w-7 object-cover' src={currentUser.avatar} alt='profile_pic' />) : (
+                        <li className='text-slate-700 hover:underline cursor-pointer'>Sign in</li>
+                        )}</Link>
             </ul>
         </div>
     </header>
