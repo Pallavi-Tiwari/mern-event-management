404bc9497be998f6c73df69dd667142ff75d9c2b
Pallavi Tiwari <pallavi6594@gmail.com>
2024-01-07 23:57:10 -0600

    updateUser api created

--- api/index.js
+++ api/index.js
@@ -2,7 +2,8 @@ import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import userRouter from './routes/user.route.js';
-import authRouter from './routes/auth.route.js'
+import authRouter from './routes/auth.route.js';
+import cookieParser from 'cookie-parser';
 dotenv.config();
 
 mongoose
@@ -16,6 +17,7 @@ mongoose
 
 const app = express();
 app.use(express.json());
+app.use(cookieParser);
 
 app.listen(5000, () => {
     console.log('Server is running on port 5000');
--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -35,7 +35,7 @@ export default function SignIn() {
         return;
       }
       dispatch(signInSuccess(data));
-      navigate('/home');
+      navigate('/');
     } catch (error) {
       dispatch(signInFailure(error.message));
     }
