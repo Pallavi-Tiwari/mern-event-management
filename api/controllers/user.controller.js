8904025a3f2e415f688a444eb7a6451d69eb4d19
Pallavi Tiwari <pallavi6594@gmail.com>
2024-01-09 18:19:01 -0600

    delete user functionality added

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -36,3 +36,14 @@ export const updateUser = async (req, res, next) => {
     next(error);
   }
 };
+
+export const deleteUser = async (req, res, next) => {
+  if(req.user.id!== req.params.id) return next(errorHandler(401,'You can only delete your own account'));
+  try {
+    await User.findByIdAndDelete(req.params.id)
+    res.status(200).json({message: 'User has been deleted successfully!'});
+    res.clearCookie('access_token');
+  } catch (error) {
+    next(error)
+  }
+};
