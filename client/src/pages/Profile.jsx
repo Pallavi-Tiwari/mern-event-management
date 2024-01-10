4debb04488befdf1f02e1882b0d252544f449349
Pallavi Tiwari <pallavi6594@gmail.com>
2024-01-09 18:20:02 -0600

    delete user functionality added -2

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -2,9 +2,10 @@ import { useRef, useState, useEffect } from 'react';
 import { useSelector } from 'react-redux';
 import { getStorage, uploadBytesResumable, ref, getDownloadURL } from 'firebase/storage';
 import { app } from '../firebase';
-import { updateUserFailure, updateUserSuccess, updateUserStart } from '../redux/user/userSlice';
+import { updateUserFailure, updateUserSuccess, updateUserStart, deleteUserFailure, deleteUserStart, deleteUserSucces } from '../redux/user/userSlice';
 import { useDispatch } from 'react-redux';
 import { Link } from 'react-router-dom';
+import { errorHandler } from '../../../api/utils/error';
 
 export default function Profile() {
   const fileRef = useRef(null);
@@ -78,6 +79,24 @@ export default function Profile() {
       dispatch(updateUserFailure(error.message));
     }
   }
+   const handleDeleteUser = async () => {
+    try {
+      dispatch(deleteUserStart());
+      const res = await fetch(`/api/user/delete/${currentUser._id}`,{
+        method: 'DELETE'
+      });
+      const data = await res.json();
+
+      if(data.success===false) {
+        dispatch(deleteUserFailure(data.message));
+        return;
+      }
+      dispatch(deleteUserSucces(data));
+
+    } catch (error) {
+      dispatch(deleteUserFailure(error.message));
+    }
+   }
 
   return (
     <div className='p-3 max-w-lg mx-auto'>
@@ -104,7 +123,7 @@ export default function Profile() {
         <button className='bg-slate-700 text-white rounded-lg p-3 uppercase hover:opacity-95 disabled:opacity-80'>Update</button>
       </form>
       <div className='flex justify-between mt-5'>
-        <span className='text-red-700 cursor-pointer'>Delete account</span>
+        <span onClick={handleDeleteUser} className='text-red-700 cursor-pointer'>Delete account</span>
         <span className='text-red-700 cursor-pointer'>Sign out</span>
       </div>
       <p className='text-red-700 mt-5'>{error ? error : ''}</p>
--- client/src/redux/user/userSlice.js
+++ client/src/redux/user/userSlice.js
@@ -33,9 +33,31 @@ const userSlice = createSlice({
         updateUserFailure: (state, action) => {
             state.error = action.payload;
             state.loading = false;
+        },
+        deleteUserStart: (state) => {
+            state.loading = true;
+        },
+        deleteUserSucces: (state) => {
+            state.currentUser= null;
+            state.loading = false;
+            state.error = null;
+        },
+        deleteUserFailure: (state, action) => {
+            state.error = action.payload;
+            state.loading = false;
         }
     }
 });
 
-export const { signInStart, signInSuccess, signInFailure, updateUserStart ,updateUserSuccess, updateUserFailure } = userSlice.actions;
+export const { 
+    signInStart, 
+    signInSuccess, 
+    signInFailure, 
+    updateUserStart,
+    updateUserSuccess, 
+    updateUserFailure,
+    deleteUserStart,
+    deleteUserSucces,
+    deleteUserFailure
+ } = userSlice.actions;
 export default userSlice.reducer;
