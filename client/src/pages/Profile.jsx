3a7f1ba5d2c543d9e60ff7bbf51448fad3da9ddb
Pallavi Tiwari <pallavi6594@gmail.com>
2024-01-11 15:43:45 -0600

    show event list completed

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -15,6 +15,8 @@ export default function Profile() {
   const [fileUploadError, setFileUploadError] = useState(false);
   const [formData, setFormData] = useState({});
   const [updateSuccess, setUpdateSuccess] = useState(false);
+  const [eventListings, setEventListings] = useState([]);
+  const [showListingError, setShowListingError] = useState(false);
   const dispatch = useDispatch();
 
   //firebase storage rules:
@@ -79,7 +81,8 @@ export default function Profile() {
       dispatch(updateUserFailure(error.message));
     }
   }
-   const handleDeleteUser = async () => {
+  
+  const handleDeleteUser = async () => {
     try {
       dispatch(deleteUserStart());
       const res = await fetch(`/api/user/delete/${currentUser._id}`,{
@@ -96,9 +99,9 @@ export default function Profile() {
     } catch (error) {
       dispatch(deleteUserFailure(error.message));
     }
-   }
+  }
 
-   const handleSignOut = async () => {
+  const handleSignOut = async () => {
     try {
     dispatch(signOutUserStart());
       const res = await fetch('/api/auth/signout');
@@ -111,7 +114,21 @@ export default function Profile() {
     } catch(error) {
       dispatch(signOutUserFailure(data.message));
     }
-   }
+  }
+
+  const handleShowListings = async () => {
+    try {
+      const res = await fetch(`/api/user/listings/${currentUser._id}`);
+      const data = await res.json();
+      if (data.success===false) {
+        setShowListingError(true);
+        return;
+      }
+      setEventListings(data);
+    } catch(error) {
+      setShowListingError(true);
+    }
+  }
   return (
     <div className='p-3 max-w-lg mx-auto'>
       <h1 className='text-3xl font-semibold text-center my-7'>Profile</h1>
@@ -145,6 +162,37 @@ export default function Profile() {
       <p className='text-green-700 mt-5'>
         {updateSuccess ? 'User is updated successfully!' : ''}
       </p>
-    </div>
-  )
+      <button onClick={handleShowListings} className='text-green-700 w-full'>Show Event List</button>
+      <p className='text-red-700 mt-5'>{showListingError ? 'Error showing event list': ''}</p>
+      {eventListings && eventListings.length > 0 && (
+         <div className='flex flex-col gap-4'>
+         <h1 className='text-center mt-7 text-2xl font-semibold'>
+           Your Event List
+         </h1>
+         {eventListings.map((listing) => (
+           <div key={listing._id} className='border rounded-lg p-3 flex justify-between items-center gap-4' >
+            <Link to={`/listing/${listing._id}`}>
+              <img
+                src={listing.imageUrls[0]}
+                alt='listing cover'
+                className='h-16 w-16 object-contain'
+              />
+            </Link>
+            <Link to={`/listing/${listing._id}`}  className='text-slate-700 font-semibold  hover:underline truncate flex-1'>
+              <p>{listing.name}</p>
+            </Link>
+            <div className='flex flex-col item-center'>
+              <button onClick={() => handleListingDelete(listing._id)} className='text-red-700 uppercase' >
+                Delete
+              </button>
+              <Link to={``}>
+                <button className='text-green-700 uppercase'>Edit</button>
+              </Link>
+            </div>
+          </div>
+         ))}
+       </div>
+     )}
+     </div>
+  );
 }
