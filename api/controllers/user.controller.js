6f8b55db3a577b4efe5a77ce8c1833c2d0f42c04
Pallavi Tiwari <pallavi6594@gmail.com>
2024-01-11 15:02:29 -0600

    get user event listing api route created and tested

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -1,5 +1,6 @@
 import bcryptjs from 'bcryptjs';
-import User from '../usermodels/user.model.js'
+import User from '../usermodels/user.model.js';
+import Listing from '../usermodels/listing.model.js';
 import { errorHandler } from '../utils/error.js';
 
 export const test = (req, res) => {
@@ -47,3 +48,16 @@ export const deleteUser = async (req, res, next) => {
     next(error)
   }
 };
+
+export const getUserListings = async (req, res, next) => {
+  if(req.user.id === req.params.id) {
+    try {
+      const listings = await Listing.find({useRef: req.params.id});
+      res.status(200).json(listings);
+    } catch(error) {
+      next(error);
+    }
+  } else {
+    return next(errorHandler(401, 'You can only see event listings created by this account!'));
+  }
+}
