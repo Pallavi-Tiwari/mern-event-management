6f7cd07775128694ba79c4b671c58124dbf3f1e7
Pallavi Tiwari <pallavi6594@gmail.com>
2024-01-19 23:12:06 -0600

    onChange and onSubmit done for search page

--- client/src/pages/Search.jsx
+++ client/src/pages/Search.jsx
@@ -11,15 +11,93 @@ export default function Search() {
       type: 'all',
       parking: false,
       offer: false,
+      ageAllowed: false,
       sort: 'created_at',
       order: 'desc',
     });
+    console.log(listings);
+    useEffect( () => {
+        const urlParams = new URLSearchParams(location.search);
+        const searchTermFromUrl = urlParams.get('searchTerm');
+        const typeFromUrl = urlParams.get('type');
+        const parkingFromUrl = urlParams.get('parking');
+        const ageAllowedFromUrl = urlParams.get('ageAllowed');
+        const offerFromUrl = urlParams.get('offer');
+        const sortFromUrl = urlParams.get('sort');
+        const orderFromUrl = urlParams.get('order');
+
+        if (
+            searchTermFromUrl ||
+            typeFromUrl ||
+            parkingFromUrl ||
+            ageAllowedFromUrl ||
+            offerFromUrl ||
+            sortFromUrl ||
+            orderFromUrl
+          ) {
+            setSidebardata({
+              searchTerm: searchTermFromUrl || '',
+              type: typeFromUrl || 'all',
+              parking: parkingFromUrl === 'true' ? true : false,
+              ageAllowed: ageAllowedFromUrl === 'true' ? true : false,
+              offer: offerFromUrl === 'true' ? true : false,
+              sort: sortFromUrl || 'created_at',
+              order: orderFromUrl || 'desc',
+            });
+    }
+
+    const fetchListings = async () =>{
+        setLoading(true);
+        setShowMore(false);
+        const searchQuery = urlParams.toString();
+        const res = await fetch(`/api/listing/get?${searchQuery}`);
+        const data = await res.json();
+        if (data.length > 8) {
+            setShowMore(true);
+        } else {
+            setShowMore(false);
+        }
+        setListings(data);
+        setLoading(false); 
+    };
+        fetchListings();
+}, [location.search]);
 
     const handleChange = (e) => {
-
+        if(e.target.id ==='all'|| e.target.id ==='social'|| e.target.id ==='private' ) {
+            setSidebardata({...sidebardata, type:e.target.id});
+        }
+        if (e.target.id === 'searchTerm') {
+            setSidebardata({ ...sidebardata, searchTerm: e.target.value });
+          }
+      
+        if ( e.target.id === 'parking' || e.target.id === 'offer'|| e.target.id === 'ageAllowed' ) {
+            setSidebardata({
+                ...sidebardata,
+                [e.target.id]:
+                e.target.checked || e.target.checked === 'true' ? true : false,
+            });
+        }
+        if (e.target.id === 'sort_order') {
+            const sort = e.target.value.split('_')[0] || 'created_at';
+            const order = e.target.value.split('_')[1] || 'desc';
+            setSidebardata({ ...sidebardata, sort, order });
+        }
     }
+
     const handleSubmit = (e) => {
         e.preventDefault();
+        const urlParams = new URLSearchParams();
+        urlParams.set('searchTerm', sidebardata.searchTerm);
+        urlParams.set('type', sidebardata.type);
+        urlParams.set('parking', sidebardata.parking);
+        urlParams.set('ageAllowed', sidebardata.ageAllowed);
+        urlParams.set('offer', sidebardata.offer);
+        urlParams.set('sort', sidebardata.sort);
+        urlParams.set('order', sidebardata.order);
+        const searchQuery = urlParams.toString();
+        console.log(searchQuery)
+        navigate(`/search?${searchQuery}`);
     }
     const onShowMore = async () => {
         const noOfListings = listings.length;
@@ -30,7 +108,12 @@ export default function Search() {
             <form onSubmit={handleSubmit} className='flex flex-col gap-8'>
                 <div className='flex items-center gap-2'>
                     <label className='whitespace-nowrap font-semibold'>Search Term:</label>
-                    <input type='text' id='searchTerm' placeholder='Search...' className='border rounded-lg p-3 w-full' />
+                    <input 
+                        type='text' 
+                        id='searchTerm' placeholder='Search...' 
+                        value={sidebardata.searchTerm}
+                        onChange={handleChange}
+                        className='border rounded-lg p-3 w-full' />
                 </div>
                 <div className='flex gap-2 flex-wrap items-center'>
                     <label className='font-semibold'>Type:</label>
@@ -80,7 +163,7 @@ export default function Search() {
                             id='parking'
                             className='w-5'
                             onChange={handleChange}
-                            checked={sidebardata.type === 'all'} />
+                            checked={sidebardata.parking} />
                         <span>Parking Lot</span>
                     </div>
                     <div className='flex gap-2'>
@@ -89,18 +172,23 @@ export default function Search() {
                             id='ageAllowed'
                             className='w-5'
                             onChange={handleChange}
-                            checked={sidebardata.type === 'ageAllowed'}
+                            checked={sidebardata.ageAllowed}
                         />
                         <span>Age Restrictions</span>
                     </div>
                 </div>
                 <div className='flex items-center gap-2'>
                     <label className='font-semibold'>Sort:</label>
-                    <select id='sort_order'>
-                        <option>Price - high to low</option>
-                        <option>Price - low to high</option>
-                        <option>Latest</option>
-                        <option>Oldest</option>
+                    <select
+                        onChange={handleChange}
+                        defaultValue={'created_at_desc'} 
+                        id='sort_order'
+                        className='border rounded-lg p-3'
+                    >
+                        <option value='regularPrice_desc'>Price - high to low</option>
+                        <option value='regularPrice_asc'>Price - low to high</option>
+                        <option value='createdAt_desc'>Latest</option>
+                        <option value='createdAt_asc'>Oldest</option>
                     </select>
                 </div>
                 <button className='bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95'>
@@ -120,11 +208,11 @@ export default function Search() {
             </p>
           )}
 
-          {!loading &&
+          {/* {!loading &&
             listings &&
             listings.map((listing) => (
               <ListingItem key={listing._id} listing={listing} />
-            ))}
+            ))} */}
 
           {showMore && (
             <button
